2b884cfbcfdb13e71b0edd5cbaf68fb15aa480e4
João Marcelo <jmgrd98@gmail.com>
2023-01-30 19:55:56 -0300

    styling app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import {AiFillRocket} from 'react-icons/ai'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Todo</h1>
+
+    <div className="logo">
+      <AiFillRocket className='rocketLogo' size={50}/>
+      <h1><span>To</span>Do</h1>
+    </div>
+
+    <div className="search">
+    <input type="text" name="" id="" placeholder='Adicione uma nova tarefa'/>
+    <button type='click'>Criar</button>
+    </div>
+
+    <main>
+
+    <header>
+      <div className="createdTodos">
+      <h2>Tarefas criadas</h2>
+      <p>0</p>
+      </div>
+
+    <div className="completedTodos">
+      <h2>Concluídas</h2>
+      <p>1 de 5</p>
+      </div>
+
+      </header>
+      
+    <hr />
+
+    </main>
+
     </div>
   )
 }
